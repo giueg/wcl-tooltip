a8507026b72286805934c28009b25dca3573e30a
kawausokun <takuro.matsukawa@gmail.com>
2015-08-14 17:40:57 +0900

    reset demo site design

---
+++ _includes/README.md
@@ -0,0 +1,67 @@
+# wcl-tooltip
+
+[![GitHub version](https://badge.fury.io/gh/kawausokun%2Fwcl-tooltip.svg)](http://badge.fury.io/gh/kawausokun%2Fwcl-tooltip)
+[![Code Climate](https://codeclimate.com/github/kawausokun/wcl-tooltip/badges/gpa.svg)](https://codeclimate.com/github/kawausokun/wcl-tooltip)
+
+{% include demo.html %}
+
+## Installation
+
+```sh
+npm install https://github.com/kawausokun/wcl-tooltip
+```
+
+## Usage
+
+```js
+window.addEventListener('load', function () {
+  var tooltip = require('wcl-tooltip');
+  tooltip.init('.i-need-tooltip', {
+    type: 'click'
+  });
+});
+```
+
+```html
+<span class="i-need-tooltip" data-wcltip-text="I'm tooltip!">data-wcltip-text</span>
+
+<span class="i-need-tooltip" data-wcltip-text-src="menu2-tooltip">data-wcltip-text-src</span>
+<span id="menu2-tooltip" style="display: none;">I'm tooltip too!</span>
+
+<span class="i-need-tooltip" data-wcltip-html-src="menu3-tooltip" data-wcltip-title="Kuma-mon">data-wcltip-html-src</span>
+<div id="menu3-tooltip" style="display: none;">
+  <img src="img/kumamon.jpg" alt="kuma-mon"><br/>
+</div>
+```
+
+## Option
+
+### init options
+
+| Option   | Values                                               | Description           |
+|----------|------------------------------------------------------|-----------------------|
+| type     | click (default)<br> hover                            | Tooltip show trigger. |
+| position | auto (default)<br>top-left<br>top<br>top-right<br>left-top<br>left<br>left-bottom<br>bottom-left<br>bottom<br>bottom-right<br>right-top<br>right<br>right-bottom     | Tooltip position.     |
+| theme    | standard (default)<br> dark                          | Tooltip look.         |
+| arrow    | true (default)<br> false                             | Show arrow.         |
+
+## License
+
+[MIT](https://github.com/kawausokun/wcl-tooltip/blob/master/LICENSE)
+
+## Author
+
+[kawausokun](https://github.com/kawausokun)
+
+## Roadmap
+
+* ajax
+* bower support
+* direct html script src
+* delay show
+* sticky theme
+
+## TODO
+
+* Add minimum test
+* min&map js
--- javascripts/bundle.js
+++ javascripts/bundle.js
@@ -1,4 +1,21 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+
+window.addEventListener('load', function() {
+    'use strict';
+
+    var tooltip = require('wcl-tooltip');
+    tooltip.init('#tooltip-menu', {
+        position: 'bottom-left',
+        arrow: false
+    });
+    tooltip.init('#tooltip-square', {
+        type: 'hover',
+        theme: 'dark'
+    });
+
+});
+
+},{"wcl-tooltip":2}],2:[function(require,module,exports){
 /*jshint multistr:true */
 /*jshint expr:true */
 
@@ -403,68 +420,4 @@
 
     module.exports = Tooltip;
 }());
-},{}],2:[function(require,module,exports){
-var sectionHeight = function() {
-    var total    = $(window).height(),
-        $section = $('section').css('height','auto');
-
-    if ($section.outerHeight(true) < total) {
-        var margin = $section.outerHeight(true) - $section.height();
-        $section.height(total - margin - 20);
-    } else {
-        $section.css('height','auto');
-    }
-}
-
-$(window).resize(sectionHeight);
-
-$(document).ready(function(){
-    $("section h1, section h2").each(function(){
-        $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
-        $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
-        $("nav ul li:first-child a").parent().addClass("active");
-    });
-
-    $("nav ul li").on("click", "a", function(event) {
-        var position = $($(this).attr("href")).offset().top - 190;
-        $("html, body").animate({scrollTop: position}, 400);
-        $("nav ul li a").parent().removeClass("active");
-        $(this).parent().addClass("active");
-        event.preventDefault();
-    });
-
-    sectionHeight();
-
-    $('img').load(sectionHeight);
-
-    var tooltip = require('wcl-tooltip');
-    tooltip.init('#tooltip-menu', {
-        position: 'bottom-left',
-        arrow: false
-    });
-    tooltip.init('#tooltip-square', {
-        type: 'hover',
-        theme: 'dark'
-    });
-});
-
-fixScale = function(doc) {
-
-    var addEvent = 'addEventListener',
-        type = 'gesturestart',
-        qsa = 'querySelectorAll',
-        scales = [1, 1],
-        meta = qsa in doc ? doc[qsa]('meta[name=viewport]') : [];
-
-    function fix() {
-        meta.content = 'width=device-width,minimum-scale=' + scales[0] + ',maximum-scale=' + scales[1];
-        doc.removeEventListener(type, fix, true);
-    }
-
-    if ((meta = meta[meta.length - 1]) && addEvent in doc) {
-        fix();
-        scales = [.25, 1.6];
-        doc[addEvent](type, fix, true);
-    }
-};
-},{"wcl-tooltip":1}]},{},[2]);
+},{}]},{},[1]);
